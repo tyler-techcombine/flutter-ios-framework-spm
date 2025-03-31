19d68e8d7c2f0948b664e6d8311197987db3f037
tyler-techcombine <saihanlinaung@techcombine.co>
2025-03-31 14:24:15 +0700

    change to debug framework

--- Package.swift
+++ Package.swift
@@ -18,12 +18,12 @@ let package = Package(
         .binaryTarget(
             name: "Flutter",
             url: "https://winter-queen-2573.saihanlinaung.workers.dev/flutter.zip",
-            checksum: "f9df78c3a3b64b7e5a950e91c30dc96f25921ac0efedd33df8d6bd3489ad55f6"
+            checksum: "2e99896348444908c76961f2a9a6a08809203ae68b58b5283439e56ab7441cf7"
         ),
         .binaryTarget(
             name: "App",
             url: "https://winter-queen-2573.saihanlinaung.workers.dev/app.zip",
-            checksum: "5e173e6a89eb51aac71d9bf0437be894eddd76cc66a09f7c797ee2982b95945a"
+            checksum: "4477d3fbaedf23a83a6b09e38aec0494aed6865a7d70b77699a1a0efd20a3074"
         )
     ]
 )
