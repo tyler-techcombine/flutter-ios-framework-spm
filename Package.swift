a993ecbb41166ec74fbd297d4b79a562c7bb1448
tyler-techcombine <saihanlinaung@techcombine.co>
2025-04-01 11:39:54 +0700

    add screen protect and pay ios xcframeworks

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "OmiseFlutterSPM",
-            targets: ["Flutter", "App"])
+            targets: ["Flutter", "App", "ScreenProtector", "PayiOS"])
     ],
     targets: [
         .binaryTarget(
@@ -24,6 +24,16 @@ let package = Package(
             name: "App",
             url: "https://winter-queen-2573.saihanlinaung.workers.dev/app.zip",
             checksum: "4477d3fbaedf23a83a6b09e38aec0494aed6865a7d70b77699a1a0efd20a3074"
+        ),
+        .binaryTarget(
+            name: "ScreenProtector",
+            url: "https://winter-queen-2573.saihanlinaung.workers.dev/screen_protector.zip",
+            checksum: "97b74b21f5dbb058b87d5282dda32a38899380aa9e19bf57e9843ea3e745a011"
+        ),
+        .binaryTarget(
+            name: "PayiOS",
+            url: "https://winter-queen-2573.saihanlinaung.workers.dev/pay_ios.zip",
+            checksum: "219c0de9d0f86b4ab1749bff0e35292b3065558c06b5ee6da347b3744fdd5310"
         )
     ]
 )
